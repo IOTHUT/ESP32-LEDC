f46d8f9cc245d55ad1315d8ca9e726f6a077fa8c
brenner <brennerbres@hotmail.com>
2022-03-04 18:18:09 -0300

    alteracao de comentario

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 
 #define LEDC_CHANNEL_0     0        // 0-15 (16 canais)
 #define LEDC_TIMER_13_BIT  13       // 13 bits de resolução
-#define LEDC_BASE_FREQ     10       // 5 kHz de clock base para o LED
+#define LEDC_BASE_FREQ     5000     // 5 kHz de clock base para o LED
 #define LED_PIN            2        // led pin
 
 int ledBrightness = 0;    // variavel para definir o brilho do led
-int stepIncrement = 5;    // degrau de incremento ou decremento para o brilho do led
+int stepIncrement = 20;    // degrau de incremento ou decremento para o brilho do led
 
 void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
-  // calcula o duty cycle (deve estar )
+  // calcula o duty cycle
   uint32_t duty = (8191 / valueMax) * min(value, valueMax);
-  ledcWrite(channel, duty);  // aplica de fato as mudanças ao pino associado ao canal
+  ledcWrite(channel, duty);  // escreve as mudanças ao pino associado ao canal
 }
 
 
@@ -27,7 +27,7 @@ void setup() {
 
 
 void loop() {
-  // "escreve" mudanças no canal do led (aplica o brilho selecionado)
+  // chama função para calcular o duty e alterar o brilho
   ledcAnalogWrite(LEDC_CHANNEL_0, ledBrightness);
 
   // altera variável brightness de acordo com os steps selecionados
@@ -41,3 +41,6 @@ void loop() {
   // altere este delay para um efeito mais rápido
   delay(30);
 }
+
+
+
